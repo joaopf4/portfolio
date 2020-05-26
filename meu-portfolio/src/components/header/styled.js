9b7f9a8ffaa55bd25cd3e895a67e90db8a46bc8d
joaopfa <joaopfa@hotmail.com>
2020-05-25 21:37:09 -0300

    primeiros trabalhos no portfólio

---
+++ meu-portfolio/src/components/header/styled.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components'
+
+export const MainDivHeader = styled.div `
+    min-width: 100%;
+    width: fit-content;
+    min-height: 190px;
+    height: fit-content;
+    background-color: #F37221;
+`
+/// Laranja claro:
+export const UpDivHeader = styled.div `
+    min-width: 100%;
+    width: fit-content;
+    height: fit-content;
+    background-color: #feab74;
+    padding: 15px 25px;
+    box-sizing:border-box;
+   
+`   
+export const CvName = styled.h1 `
+    margin: auto 15px auto auto;
+    @font-face {
+    font-family: 'nexa_bold.otf';
+    src: url("../header/fonts/nexa_bold.otf");
+    }
+`
+export const NameAndUnderWrapper = styled.div ``
+export const NameAndQrWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+export const CvUnderName = styled.h3 `
+    margin: 0;
+`
+export const QrCodeLIn = styled.img `
+    width:80px;
+    margin: auto;
+`
+////Laranja escuro 
+export const DownDivHeader = styled.div `
+    width: 100%;
+    display: grid; 
+    grid-template-columns: 1fr 1fr 1fr;
+`
+export const ContactsDiv = styled.div `
+    padding: 25px; 
+    grid-column: 1;
+`
+export const LittleInfo = styled.p `
+    margin: 3px;
+    font-weight: 700;
+`
