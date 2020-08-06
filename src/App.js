aa5c27fe392bd7961a0cd976b68ec7d3293a8324
joaopfa <joaopfa@hotmail.com>
2020-08-05 23:05:12 -0300

    comentando heder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Header from './Components/Header'
+//import Header from './Components/Header'
 import Main from './Components/Main'
 import styled from 'styled-components'
 
 function App() {
   return (
     <Body >
-       <Header/> 
+       {/* <Header/>  */}
       <Main/>
     </Body>
   );
