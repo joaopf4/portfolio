f2f069e5e1c05415902b219b9687c2f5af026079
joaopfa <joaopfa@hotmail.com>
2020-08-07 01:43:50 -0300

    tentativa de extrair first section para outro componente mas algo deu errado. Tbm não consegui repontuar os arquivos de .js para .jsx

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -9,46 +9,48 @@ import Habilidades from '../Skills';
 import Projetos from '../Projects';
 import Contato from '../Contact';
 import Footer from '../Footer';
+//import FirstSection from '../FirstSection/index';
 
-    class Main extends React.Component {
-        render() {
-            return (
-                <MainBody>
-                    <FirstSection id="eujoao">
-                        <FSLeft>
-                            <FSLTxt>
-                                <h1>Eu sou o João Pedro!</h1>
-                                <h2>Desenvolvedor Web Full Stack</h2>
-                                <p> ReactJS | HTML | CSS | JavaScript | TypeScript | MySQL | AWS | Firebase</p>
-                                <a href={Curriculo} download = "curriculo_joaopedro.pdf"><Buttons>CV em PDF <i class="fa fa-download"></i></Buttons></a>
-                            </FSLTxt>
-                            <IconsWrapperL>
-                                <IconLinks href='http://www.instagram.com/joaopfa' target="blank"><i class="fab fa-instagram"></i> </IconLinks>
-                                <IconLinks href='http://www.linkedin.com/in/joaopfa' target="blank"><i class="fab fa-linkedin"></i></IconLinks>
-                                <IconLinks href='https://github.com/joaopf4' target="blank"><i class="fab fa-github"></i></IconLinks>
-                                <IconLinks href='https://api.whatsapp.com/send?phone=+5531988165740' target="blank"><i class="fab fa-whatsapp"></i></IconLinks>
-                            </IconsWrapperL>    
-                        </FSLeft>
+class Main extends React.Component {
+    render() {
+        return (
+            <MainBody>
+                <FirstSection id="eujoao">
+                    <FSLeft>
+                        <FSLTxt>
+                            <h1>Eu sou o João Pedro!</h1>
+                            <h2>Desenvolvedor Web Full Stack</h2>
+                            <p> ReactJS | HTML | CSS | JavaScript | TypeScript | MySQL | AWS | Firebase</p>
+                            <a href={Curriculo} download = "curriculo_joaopedro.pdf"><Buttons>CV em PDF <i class="fa fa-download"></i></Buttons></a>
+                        </FSLTxt>
+                        <IconsWrapperL>
+                            <IconLinks href='http://www.instagram.com/joaopfa' target="blank"><i class="fab fa-instagram"></i> </IconLinks>
+                            <IconLinks href='http://www.linkedin.com/in/joaopfa' target="blank"><i class="fab fa-linkedin"></i></IconLinks>
+                            <IconLinks href='https://github.com/joaopf4' target="blank"><i class="fab fa-github"></i></IconLinks>
+                            <IconLinks href='https://api.whatsapp.com/send?phone=+5531988165740' target="blank"><i class="fab fa-whatsapp"></i></IconLinks>
+                        </IconsWrapperL>    
+                    </FSLeft>
 
-                        <FSRight>
-                            <IconsWrapperR>                                
-                                <IconLinks href='http://www.instagram.com/joaopfa' target="blank"><i class="fab fa-instagram"></i> </IconLinks>
-                                <IconLinks href='http://www.linkedin.com/in/joaopfa' target="blank"><i class="fab fa-linkedin"></i></IconLinks>
-                                <IconLinks href='https://github.com/joaopf4' target="blank"><i class="fab fa-github"></i></IconLinks>
-                                <IconLinks href='https://api.whatsapp.com/send?phone=+5531988165740' target="blank"><i class="fab fa-whatsapp"></i></IconLinks>
-                            </IconsWrapperR>
-                        </FSRight>
-                    </FirstSection>
-                        <SobreMim/>
-                        <Habilidades/>    
-                        <Educação/>
-                        <Projetos/>
-                        <Contato/>
-                        <Footer/>
+                    <FSRight>
+                        <IconsWrapperR>                                
+                            <IconLinks href='http://www.instagram.com/joaopfa' target="blank"><i class="fab fa-instagram"></i> </IconLinks>
+                            <IconLinks href='http://www.linkedin.com/in/joaopfa' target="blank"><i class="fab fa-linkedin"></i></IconLinks>
+                            <IconLinks href='https://github.com/joaopf4' target="blank"><i class="fab fa-github"></i></IconLinks>
+                            <IconLinks href='https://api.whatsapp.com/send?phone=+5531988165740' target="blank"><i class="fab fa-whatsapp"></i></IconLinks>
+                        </IconsWrapperR>
+                    </FSRight>
+                </FirstSection> 
+                    {/* <FirstSection/>*/}
+                    <SobreMim/>
+                    <Habilidades/>    
+                    <Educação/>
+                    <Projetos/>
+                    <Contato/>
+                    <Footer/>
 
-                </MainBody>
-                );
-            }
-          }
+            </MainBody>
+            );
+        }
+    }
 
-          export default Main;
+export default Main;
