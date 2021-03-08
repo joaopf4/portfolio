0709c1fca271446ccc907a20957c5bafd6fbb145
Joao Pedro <joaopfa@hotmail.com>
2021-03-08 16:31:16 -0300

    deleting old project list code

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -53,9 +53,7 @@ class Projects extends React.Component {
                 },
             ]
         };
-        {console.log(this.state.projects)}
     }
-
 
     render() {
         return (        
@@ -79,88 +77,7 @@ class Projects extends React.Component {
                         />
                     )
                 })}
-                    {/* <SingleProject>
-                        <ThumbNail>
-                            <img alt="muckup-gif" src={CyclingQuiz}/>
-                        </ThumbNail>
-                        <ProjectText>
-                            <h1>Cycling Quiz</h1>
-                            <p>
-                                Front-end
-                            </p>
-                            <p>
-                                Projeto feito durante a imersão Allura em NextJS, onde fizemos um quiz utilizando NextJS como framework. 
-                                Personalizei o tema do quiz com meu principal Hobbie que é bicicleta!
-                            </p>
-                            <p>
-                            <a href="https://imersao-alura-nextjs.joaopf4.vercel.app/" target="blank">Deploy da aplicação</a> 
-                            <a href="https://github.com/joaopf4/imersao-alura-nextjs" target="blank">Repositório   <i class="fab fa-github"></i></a> 
-                            
-                            </p>
-                        </ProjectText>
-                    </SingleProject>
-                    <SingleProject>
-                        <ThumbNail>
-                            <img alt="muckup-gif" src={Whats4}/>
-                        </ThumbNail>
-                        <ProjectText>
-                            <h1>Whats4</h1>
-                            <p>
-                                Front-end
-                            </p>
-                            <p>
-                                Projeto de uma reprodução simples do WhatsApp. Um produto mínimo 
-                                viável de uma aplicação de chat de mensagens.
-                            </p>
-                            <p>
-                            <a href="http://bizarre-cook.surge.sh/" target="blank">Deploy da aplicação</a> 
-                            <a href="https://github.com/joaopf4/whatsReplica" target="blank">Repositório   <i class="fab fa-github"></i></a> 
-                            
-                            </p>
-                        </ProjectText>
-                    </SingleProject>
-                    <SingleProject>
-                        <ThumbNail>
-                            <img alt="muckup-gif" src={FutureEats}/>
-                        </ThumbNail>
-                        <ProjectText>
-                            <h1>Future Eats</h1>
-                            <p>
-                                Front-end
-                            </p>
-                            <p>
-                                Projeto de uma reprodução do UberEats, com funções de cadastro, login, proteção da tela de feed,
-                                uso de redux, routes e actions.  
-                            </p>
-                            <p>
-                            <a href="http://projeto-final-hamilton-future-eats.surge.sh/" target="blank">Deploy da aplicação</a> 
-                            <a href="https://github.com/joaopf4/futureEats" target="blank">Repositório   <i class="fab fa-github"></i></a> 
-                            
-                            </p>
-                        </ProjectText>
-                    </SingleProject>
-                    <SingleProject>
-                        <ThumbNail>
-                            <img alt="muckup-gif" src={Eddit}/>
-                        </ThumbNail>
-                        <ProjectText>
-                            <h1>4Eddit</h1>
-                            <p>
-                                Front-end
-                            </p>
-                            <p>
-                                Projeto de uma reprodução de um modelo simplificado de funcionamento e consumo 
-                                de uma API que se assemelha a aplicação do Reddit, onde posts podem receber likes/unlikes e comentários,
-                                através da integração com uma API.
-                            </p>
-                            <p>
-                            <a href="http://furry-control.surge.sh/" target="blank">Deploy da aplicação</a> 
-                            <a href="https://github.com/joaopf4/4eddit" target="blank">Repositório   <i class="fab fa-github"></i></a> 
-                            
-                            </p>
-                        </ProjectText>
-                    </SingleProject> */}
-                    <MoreProjects><a href="https://github.com/joaopf4?tab=repositories" target="blank">Mais projetos</a>   <i class="fab fa-github"></i></MoreProjects>
+                <MoreProjects><a href="https://github.com/joaopf4?tab=repositories" target="blank">Mais projetos</a>   <i class="fab fa-github"></i></MoreProjects>
             </ProjectsWrapper>
         </Section>
         );
