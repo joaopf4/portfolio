221c05ac7c1a1445a4249891e2fc0e35a82e0bbd
Joao Pedro <joaopfa@hotmail.com>
2021-04-15 19:19:09 -0300

    trailes >> trailer

--- src/components/Projects/strings.js
+++ src/components/Projects/strings.js
@@ -1,6 +1,6 @@
 export const ProjectsStrings = {
     projectDescription: [
-        {id: 0, description: 'Projeto onde integro a interface a uma API contendo uma lista de filmes, onde o usuário pode ver detalhes de cada filme, salvar algum em uma lista de favoritos e ir direto para o trailes de um deles.'},
+        {id: 0, description: 'Projeto onde integro a interface a uma API contendo uma lista de filmes, onde o usuário pode ver detalhes de cada filme, salvar algum em uma lista de favoritos e ir direto para o trailer de um deles.'},
         {id: 1, description: 'Projeto no qual me desafiei a recriar uma aplicação famosa que é o Strava! Utilizei ReactJS para a mesma, e sigo melhorando pouco a pouco essa reprodução do Strava, tentando dar mais efeitos visuais similares a aplicação original!'},
         {id: 2, description: 'Projeto feito durante a imersão Allura em NextJS, onde fizemos um quiz utilizando NextJS como framework. Personalizei o tema do quiz com meu principal Hobbie que é bicicleta!'},
         {id: 3, description: 'Projeto de uma reprodução simples do WhatsApp. Um produto mínimo viável de uma aplicação de chat de mensagens.'},
