8b8af7fc57ff81b3b62965477ce492d29f03a510
Joao Pedro <joaopfa@hotmail.com>
2021-05-05 14:48:31 -0300

    atualizando idade na descriçao

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -20,8 +20,8 @@ class AboutMe extends React.Component {
                         Muito prazer! Me chamo João Pedro, e é uma honra recebê-lo em meu portfólio!  
                         </p>
                         <p>
-                        Sou um jovem Belorizontino a poucos passos de entrar na casa dos 30, e que vem 
-                        dedicando o seu último ano ao estudo de desenvolvimento web full-stack. 
+                        Sou um jovem Belorizontino recém chegado aos 30 anos, e que vem 
+                        dedicando o seu último ano ao estudo de desenvolvimento web. 
                         </p>
                         <p>
                         Com formação em Publicidade e Propaganda e experiência que vai desde Gerente de Hostel à organizador
@@ -30,12 +30,15 @@ class AboutMe extends React.Component {
                         </p>
                         <p>                                    
                         Para isso, em 2019 participei do mês de imersão com a <a href="https://gama.academy/" target="blank">Gama Academy</a>,
-                        e agora em 2020 dediquei o meu primeiro semestre ao curso de desenvolvimento web full-stack da 
+                        e em 2020 dediquei o meu primeiro semestre ao curso de desenvolvimento web full-stack da 
                         <a href="https://www.labenu.com.br" target="blank"> Escola Labenu</a>, onde tive a oportunidade de criar aplicações desafiadoras 
                         e edificantes para o meu conhecimento.
                         </p>
                         <p>                                    
-                        Para além dos códigos, atualmente dedico minhas horas vagas ao ciclismo e aos meus projetos 
+                        Em 2021 venho estudando React e suas complexidades através de cursos da udemy e imersçoes de canais como Alura e RocketSeat.
+                        </p>
+                        <p>                                    
+                        Para além dos códigos, dedico minhas horas vagas ao ciclismo e aos meus projetos 
                         <a href='http://www.instagram.com/girorua' target="blank"> Giro Rua </a> e
                         <a href='http://www.instagram.com/cicloficinaderua' target="blank"> Cicloficina de Rua</a>, por onde atuo no fomento
                         à ciclomobilidade e à ciclocultura em minha cidade =).
